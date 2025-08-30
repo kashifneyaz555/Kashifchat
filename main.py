070797c8128dcc475bd6fa65569b4828cc3228eb
kashifneyaz555 <kashifneyaz5@gmail.com>
2025-08-31 00:32:18 +0530

    Allow Werkzeug for production use

--- main.py
+++ main.py
@@ -131,4 +131,4 @@ def handle_profile_picture(data):
     }, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
